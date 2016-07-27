57e11679da66ac523e1a968e467a96d5ca6795c1
Miigon <miigon.pg@gmail.com>
2016-07-27 13:12:28 +0800

    Fix bug that may allow SQL Injection Attacks

--- backend/database.js
+++ backend/database.js
@@ -6,14 +6,14 @@ database.open = function(filename){
 	database.db = new (require('sqlite3').verbose()).Database('./data.db');
 	if(!require("fs").existsSync(filename)){
 		database.db.serialize(function() {
-			database.db.run("CREATE TABLE scores (token TEXT, score INT)");
+			database.db.run("CREATE TABLE scores (token TEXT, score INT);");
 		});
 	}
 };
 
 database.addToken = function(token){
 	database.db.serialize(function() {	 
-		var addToken = database.db.prepare("INSERT INTO scores VALUES (?, 0)");
+		var addToken = database.db.prepare("INSERT INTO scores VALUES (?, 0);");
 		addToken.run(token);
 		addToken.finalize();
 	});
@@ -22,7 +22,8 @@ database.addToken = function(token){
 //callback: function(score). score will be -1 if the token does not exist.
 database.getScoreByToken = function(token,callback){
 	database.db.serialize(function() {	 
-	  database.db.get("SELECT score FROM scores WHERE token='" + token + "'", function(err, row) {
+	var getToken = database.db.prepare("SELECT score FROM scores WHERE token=?;")
+	  getToken.get(token, function(err, row) {
 		if(row){
 			callback(row.score);
 		}else{
